782f3d49cfc2b1269564ae2ca39e2da678f7e1ee
Sirius <imc.tapasdash@gmail.com>
2025-09-28 13:23:41 +0530

    Initiated Note Card feature. Need more work.

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,18 @@
 import Header from "./components/Header/Header"
+import NoteCard from "./components/NoteCard/NoteCard"
 
 const App = () => {
   return (
     <>
       <Header />
+      <div className="container">
+        <div className="note-card-wrap">
+          <NoteCard />
+          <NoteCard />
+          <NoteCard />
+          <NoteCard />
+        </div>
+      </div>
     </>
   )
 }
---
+++ src/components/NoteCard/NoteCard.jsx
@@ -0,0 +1,11 @@
+import "./NoteCard.css";
+
+const NoteCard = () => {
+  return (
+    <div className="note-card">
+      <p>The sun is so bright today. I would like to take a cup of coffee.</p>
+    </div>
+  )
+}
+
+export default NoteCard;
